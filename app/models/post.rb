69a69621218a34760d33a638eaa9444d5e8caea3
Alban Peignier <alban@tryphon.eu>
2012-07-22 19:48:04 +0200

    Add liquid_method Post#created_at

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ActiveRecord::Base
   belongs_to :show
   validates_presence_of :show_id
 
-  liquid_methods :title, :description
+  liquid_methods :title, :description, :created_at
 
   named_scope :last_updated, :order => 'updated_at desc', :limit => 5
 end
