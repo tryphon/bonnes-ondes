39afb898d6cf9bd050af9a710ecd36225b7ef1a0
Alban Peignier <alban@tryphon.eu>
2012-07-22 19:47:39 +0200

    Add support for larg thumbnail

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,7 +3,7 @@ class Image < ActiveRecord::Base
   liquid_methods :width, :height, :title
 
   belongs_to :show
-  has_attachment :storage => :file_system, :max_size => 300.kilobytes, :content_type => :image, :thumbnails => { :normal => '200>', :thumb => '75' }
+  has_attachment :storage => :file_system, :max_size => 1.megabyte, :content_type => :image, :thumbnails => { :larg => '500>', :normal => '200>', :thumb => '75' }
   validates_as_attachment
 
   protected
@@ -29,11 +29,19 @@ class Image::LiquidDropClass
     view.image_path @object.public_filename(:thumb)
   end
 
+  def url_for_larg
+    view.image_path @object.public_filename(:larg)
+  end
+
   def tag
     view.image_tag(@object.public_filename(:normal), :title => @object.title)
   end
 
   def tag_for_thumb
     view.image_tag(@object.public_filename(:thumb), :title => @object.title)
+  end
+
+  def tag_for_larg
+    view.image_tag(@object.public_filename(:larg), :title => @object.title)
   end
 end
