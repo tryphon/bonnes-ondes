c4ab0e381280620763eec3eb8a5edc252023b7a0
Alban Peignier <alban@tryphon.eu>
2012-08-01 20:59:44 +0200

    Improve sizes in episodes/edit form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -109,7 +109,7 @@ module ApplicationHelper
   end
 
   def textile_editor(*args)
-    (super *args) + '<br/><div class="textile_editor_help">Ajouter un lien: "Bonnes Ondes":http://bonnes-ondes.fr</div>'.html_safe
+    (super *args) + '<br/><div class="hint">Ajouter un lien: "Bonnes Ondes":http://bonnes-ondes.fr</div>'.html_safe
   end
 
   def textilize_in_text(content)
