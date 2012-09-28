0863be868c69201afe8c38058fab12e6b2b8a829
Alban Peignier <alban@tryphon.eu>
2012-09-28 17:18:25 +0200

    Fixes 'too large' image size

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -7,7 +7,7 @@ describe Image do
   end
 
   it "should have a french message when size is too large" do
-    @image.size = 2.megabyte
+    @image.size = 2.megabyte + 1
     @image.error_on(:size).first.should match(/fichier.*trop grand/)
   end
 
