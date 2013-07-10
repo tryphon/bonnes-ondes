4b795287b4e24f4db649abd4c3aeefc8ee9bfec6
Alban Peignier <alban@tryphon.eu>
2013-07-10 10:20:05 +0200

    Fixes /show path in url_for_show

--- config/routes.rb
+++ config/routes.rb
@@ -44,7 +44,8 @@ ActionController::Routing::Routes.draw do |map|
   # You can have the root of your site routed by hooking up ''
   # -- just remember to delete public/index.html.
   map.connect '', :controller => "public", :action => "welcome"
-  #map.connect '', :controller => "public", :action => "show"
+  # prevent /show path in url_for_show
+  map.connect '', :controller => "public", :action => "show"
 
   map.connect '/:action', :controller => "public"
 
