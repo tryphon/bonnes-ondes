48c035f87dc9f1721c02100a0a40cb93ce8ee05f
Alban Peignier <alban@tryphon.eu>
2013-07-30 08:32:45 +0200

    Improve links in textilize plain text. Refs #852

--- config/initializers/textilize.rb
+++ config/initializers/textilize.rb
@@ -1,1 +1,26 @@
 require 'red_cloth_formatters_plain'
+
+module RedCloth
+  module Formatters
+    module Plain
+
+      # render link name followed by (url)
+      def link(opts)
+        name = opts[:name]
+        url = opts[:href]
+
+        # Remove mailto: prefix
+        if url =~ /^mailto:(.*)$/
+          url = $1
+        end
+
+        if name != url
+          "#{name} (#{url})"
+        else
+          name
+        end
+      end
+
+    end
+  end
+end
