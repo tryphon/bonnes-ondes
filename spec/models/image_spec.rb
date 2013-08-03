d0b939d6674a3253f2a3a7ef13cb02a337808d6c
Alban Peignier <alban@tryphon.eu>
2013-08-03 16:43:16 +0200

    Fixes image size spec

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -3,11 +3,11 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe Image do
 
   before(:each) do
-    @image = Image.new    
+    @image = Image.new
   end
 
   it "should have a french message when size is too large" do
-    @image.size = 2.megabyte + 1
+    @image.size = 5.megabyte + 1
     @image.error_on(:size).first.should match(/fichier.*trop grand/)
   end
 
