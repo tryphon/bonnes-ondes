1da9d098346fbbc49779e22c3ce929b0f74a6be2
Alban Peignier <alban@tryphon.eu>
2013-09-15 11:40:25 +0200

    Use less files in dragonfly migration. Refs #888

--- db/migrate/20130903122559_migrate_images_to_dragonfly.rb
+++ db/migrate/20130903122559_migrate_images_to_dragonfly.rb
@@ -1,8 +1,10 @@
 class MigrateImagesToDragonfly < ActiveRecord::Migration
   def self.up
     Image.find_each do |image|
-      image.content = File.new image.full_filename
-      image.save!
+    	File.open(image.full_filename, "r") do |f|
+	      image.content = f
+	      image.save!
+	    end
     end
   end
 
