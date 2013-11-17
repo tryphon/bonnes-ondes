98d39a198d2e2c4ec325b4ddc9d38a484bb690b2
Alban Peignier <alban@tryphon.eu>
2013-11-17 13:24:57 +0100

    Use i18n for WillPaginate

--- config/initializers/will_paginate_french.rb
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-require 'will_paginate/array'
-
-WillPaginate::ViewHelpers.pagination_options[:prev_label] = '&laquo; Précédent'
-WillPaginate::ViewHelpers.pagination_options[:next_label] = 'Suivant &raquo;'
