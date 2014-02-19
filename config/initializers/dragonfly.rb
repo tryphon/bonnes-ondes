93de1fcde1fc588fb2c0cacb55bf23b7776f51a7
Alban Peignier <alban@tryphon.eu>
2014-02-19 11:08:26 +0100

    DragonFly doesn't like Pathname but String

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -6,7 +6,7 @@ images_storage_dir = Rails.root + 'storage/images'
 FileUtils.mkdir_p images_storage_dir
 
 app.datastore.configure do |d|
-  d.root_path = images_storage_dir
+  d.root_path = images_storage_dir.to_s
   # d.server_root = '/filesystem/path/public'       # filesystem root for serving from - default to nil
 end
 
